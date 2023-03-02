f03e8a0cdf0e7d8b2d19c83718c1fe9a7d952577
sfitzel <sfitzel@hotmail.com>
2023-03-02 14:24:38 -0700

    font size increase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,9 +14,6 @@
           alt=""
           loading="lazy" />
       </a>
-
-
-		<!-- <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg> -->
 	  <span class="font-semibold text-xl tracking-tight">Downhole Testing</span>
 	</div>
 	<div class="block lg:hidden">
@@ -26,10 +23,10 @@
 	</div>
 	<div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
 	  <div class="text-sm lg:flex-grow">
-		<a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-white mr-4">
+		<a href="/" class="block mt-4 lg:inline-block lg:mt-0 font-medium text-blue-600 hover:text-white mr-4">
 		  Home
 		</a>
-		<a href="/tech" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-white mr-4">
+		<a href="/tech" class="block mt-4 lg:inline-block lg:mt-0 font-medium text-blue-600 hover:text-white mr-4">
 		  Our Technology
 		</a>
 
