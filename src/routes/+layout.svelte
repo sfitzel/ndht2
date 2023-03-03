a234a28579fe8c29ffd4c40979e6dea116ae323e
sfitzel <sfitzel@hotmail.com>
2023-03-02 22:19:56 -0700

    working on tech page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,11 +22,11 @@
 	  </button>
 	</div>
 	<div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-	  <div class="text-sm lg:flex-grow">
-		<a href="/" class="block mt-4 lg:inline-block lg:mt-0 font-medium text-blue-600 hover:text-white mr-4">
+	  <div class="text-lg font-medium lg:flex-grow">
+		<a href="/" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-white mr-4">
 		  Home
 		</a>
-		<a href="/tech" class="block mt-4 lg:inline-block lg:mt-0 font-medium text-blue-600 hover:text-white mr-4">
+		<a href="/tech" class="block mt-4 lg:inline-block lg:mt-0 text-blue-600 hover:text-white mr-4">
 		  Our Technology
 		</a>
 
--- src/routes/tech/+page.svelte
+++ src/routes/tech/+page.svelte
@@ -1,3 +1,358 @@
 <h1>
     Tech page
   </h1>
+
+  <div class="container mx-auto px-4 mb-14">
+    <div
+      class="flex flex-col rounded-lg bg-white shadow-lg dark:bg-neutral-700 md:max-w-xl md:flex-row">
+      <img
+        class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg"
+        alt="" />
+      <div class="flex flex-col justify-start p-3 py-0">
+        <h5
+          class="mb-1 text-lg font-medium text-neutral-800 dark:text-neutral-50">
+          Specifications
+        </h5>
+        <table class="table-auto text-sm">
+          <tbody>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Min casing size    </td>
+              <td>4-1/2" (114 mm)</td>
+            </tr>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Max casing size</td>
+              <td>13-3/8" (341 mm)</td>
+            </tr>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Tool o.d.</td>
+              <td>3.6" (91.4 mm)</td>
+            </tr>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Tool length</td>
+              <td>45.3 ft (13.8 m)</td>
+            </tr>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Max Pressure</td>
+              <td>10,000 psi (68.9 Mpa)</td>
+            </tr>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Max Temp</td>
+              <td>350 degF (177 degC)</td>
+            </tr>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Pump rate</td>
+              <td>0.03 - 0.5 gal/min<br> 0.1 - 2.0 l/min</td>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Max pump head</td>
+              <td>1000 psi (6.9 Mpa)</td>
+            </tr>
+            <tr class="odd:bg-gray-100 hover:!bg-stone-200">
+              <td class="p-0.5 border-b text-left">Packer spacing</td>
+              <td>11 ft (3.4 m) with optional MPL tool (variable)</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <section class="dark:bg-gray-800 dark:text-gray-100">
+    <div class="container flex flex-col-reverse mx-auto lg:flex-row">
+      <div class="flex flex-col px-6 py-8 space-y-6 rounded-sm sm:p-8 lg:p-12 lg:w-1/2 xl:w-2/5 dark:bg-violet-400 dark:text-gray-900">
+        <div class="flex space-x-2 sm:space-x-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+          </svg>
+          <div class="space-y-2">
+            <p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+            <p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+          </div>
+        </div>
+        <div class="flex space-x-2 sm:space-x-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+          </svg>
+          <div class="space-y-2">
+            <p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+            <p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+          </div>
+        </div>
+        <div class="flex space-x-2 sm:space-x-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="flex-shrink-0 w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+          </svg>
+          <div class="space-y-2">
+            <p class="text-lg font-medium leading-snug">Lorem ipsum dolor sit amet</p>
+            <p class="leading-snug">Praesentium ea et neque distinctio quas eius repudiandae quaerat obcaecati voluptatem similique!</p>
+          </div>
+        </div>
+      </div>
+      <div class="lg:w-1/2 xl:w-3/5 dark:bg-gray-800">
+        <div class="flex items-center justify-center p-4 md:p-8 lg:p-12">
+          <img src="https://source.unsplash.com/640x480/" alt="" class="rounded-lg shadow-lg dark:bg-gray-500 aspect-video sm:min-h-96">
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- Container for demo purpose -->
+<div class="container my-24 px-6 mx-auto">
+
+  <!-- Section: Design Block -->
+  <section class="mb-32 text-gray-800">
+    <div class="flex flex-wrap items-center">
+      <div class="grow-0 shrink-0 basis-auto w-full lg:w-4/12 mb-6 md:mb-0 px-3">
+        <h2 class="text-3xl font-bold mb-6">
+          Why is it so<u class="text-blue-600"> great?</u>
+        </h2>
+
+        <p class="text-gray-500 mb-12">
+          Nunc tincidunt vulputate elit. Mauris varius purus malesuada neque iaculis malesuada.
+          Aenean gravida magna orci, non efficitur est porta id. Donec magna diam.
+        </p>
+      </div>
+
+      <div class="grow-0 shrink-0 basis-auto w-full lg:w-8/12 mb-6 mb-md-0 px-3">
+        <div class="flex flex-wrap">
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-12">
+            <div class="flex">
+              <div class="shrink-0 mt-1">
+                <svg class="w-4 h-4 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z">
+                  </path>
+                </svg>
+              </div>
+              <div class="grow ml-4">
+                <p class="font-bold mb-1">Support 24/7</p>
+                <p class="text-gray-500">
+                  Pellentesque mollis, metus nec fringilla aliquam. Donec consequat orci quis
+                  volutpat imperdiet.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-12">
+            <div class="flex">
+              <div class="shrink-0 mt-1">
+                <svg class="w-4 h-4 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z">
+                  </path>
+                </svg>
+              </div>
+              <div class="grow ml-4">
+                <p class="font-bold mb-1">Tracking</p>
+                <p class="text-gray-500">
+                  Magna lacus iaculis elit, quis pharetra varius. Aenean lectus ex, placerat id
+                  tellus in eros.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-12">
+            <div class="flex">
+              <div class="shrink-0 mt-1">
+                <svg class="w-4 h-4 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z">
+                  </path>
+                </svg>
+              </div>
+              <div class="grow ml-4">
+                <p class="font-bold mb-1">Reporting</p>
+                <p class="text-gray-500">
+                  Pellentesque varius ex vel consequat quis. Sed mauris ex, imperdiet sit amet
+                  nisl ac, ultrices.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-12">
+            <div class="flex">
+              <div class="shrink-0 mt-1">
+                <svg class="w-4 h-4 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z">
+                  </path>
+                </svg>
+              </div>
+              <div class="grow ml-4">
+                <p class="font-bold mb-1">Analytics</p>
+                <p class="text-gray-500">
+                  Vestibulum gravida iaculis nisl, vel lobortis eros. Praesent vulputate lacus
+                  bibendum augue.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-12">
+            <div class="flex">
+              <div class="shrink-0 mt-1">
+                <svg class="w-4 h-4 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z">
+                  </path>
+                </svg>
+              </div>
+              <div class="grow ml-4">
+                <p class="font-bold mb-1">Huge community</p>
+                <p class="text-gray-500">
+                  Praesent vulputate lacus bibendum augue. Pellentesque mollis, metus nec
+                  fringilla aliquam.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-12">
+            <div class="flex">
+              <div class="shrink-0 mt-1">
+                <svg class="w-4 h-4 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z">
+                  </path>
+                </svg>
+              </div>
+              <div class="grow ml-4">
+                <p class="font-bold mb-1">Easy to use</p>
+                <p class="text-gray-500">
+                  Sed mauris ex, imperdiet sit amet nisl ac, ultrices. Magna lacus iaculis elit,
+                  quis pharetra varius.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-12">
+            <div class="flex">
+              <div class="shrink-0 mt-1">
+                <svg class="w-4 h-4 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z">
+                  </path>
+                </svg>
+              </div>
+              <div class="grow ml-4">
+                <p class="font-bold mb-1">Frequent updates</p>
+                <p class="text-gray-500">
+                  Aenean lectus ex, placerat id tellus in eros. Pellentesque varius ex vel
+                  consequat quis. Sed mauris ex
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 mb-12">
+            <div class="flex">
+              <div class="shrink-0 mt-1">
+                <svg class="w-4 h-4 text-blue-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                  <path fill="currentColor"
+                    d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z">
+                  </path>
+                </svg>
+              </div>
+              <div class="grow ml-4">
+                <p class="font-bold mb-1">Responsive</p>
+                <p class="text-gray-500">
+                  Donec consequat orci quis volutpat imperdiet. Vestibulum gravida iaculis nisl,
+                  vel lobortis eros.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Section: Design Block -->
+
+</div>
+<!-- Container for demo purpose -->
+
+
+<section>
+  <div
+    class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
+  >
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div
+        class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
+      >
+        <img
+          alt="Party"
+          src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div class="lg:py-24">
+        <h2 class="text-3xl font-bold sm:text-4xl">Grow your audience</h2>
+
+        <p class="mt-4 text-gray-600">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic
+          atque tenetur quis eius quos ea neque sunt, accusantium soluta minus
+          veniam tempora deserunt? Molestiae eius quidem quam repellat.
+        </p>
+
+        <a
+          href="#"
+          class="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+        >
+          Get Started Today
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section>
+  <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+    <div class="max-w-3xl">
+      <h2 class="text-3xl font-bold sm:text-4xl">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod alias
+        doloribus impedit.
+      </h2>
+    </div>
+
+    <div class="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div class="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+        <img
+          alt="Party"
+          src="https://images.unsplash.com/photo-1496843916299-590492c751f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1771&q=80"
+          class="absolute inset-0 h-full w-full object-cover"
+        />
+      </div>
+
+      <div class="lg:py-16">
+        <article class="space-y-4 text-gray-600">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui
+            hic atque tenetur quis eius quos ea neque sunt, accusantium soluta
+            minus veniam tempora deserunt? Molestiae eius quidem quam repellat.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            explicabo quidem voluptatum voluptas illo accusantium ipsam quis,
+            vel mollitia? Vel provident culpa dignissimos possimus, perferendis
+            consectetur odit accusantium dolorem amet voluptates aliquid,
+            ducimus tempore incidunt quas. Veritatis molestias tempora
+            distinctio voluptates sint! Itaque quasi corrupti, sequi quo odit
+            illum impedit!
+          </p>
+        </article>
+      </div>
+    </div>
+  </div>
+</section>
